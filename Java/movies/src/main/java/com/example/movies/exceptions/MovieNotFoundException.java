ea95bef3c1929eee6af9f535549120a01dfdec3c
KacperKwiat <s24533@pjwstk.edu.pl>
2023-01-30 18:29:56 +0100

    clear

--- Java/movies/src/main/java/com/example/movies/exceptions/MovieNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.movies.exceptions;
-
-public class MovieNotFoundException extends RuntimeException {
-    public MovieNotFoundException(Long id) {
-        super("Didnt find the movie of id "+id);
-    }
-}
